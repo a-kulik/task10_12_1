0e12a6897903359940208e168c908c582beff6d2
helsik <mymail.kulik@gmail.com>
2018-05-14 13:08:57 +0300

    Update task10_12_1.sh

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -31,7 +31,7 @@ sed -i "s@manag_mask@$MANAGEMENT_NET_MASK@" ${dir_pwd}/config-drives/vm1-config/
 sed -i "s@manag_broad@${MANAGEMENT_NET}.255@" ${dir_pwd}/config-drives/vm1-config/meta-data
 sed -i "s@dns_vm@$VM_DNS@" ${dir_pwd}/config-drives/vm1-config/meta-data
 # VM1 user-data
-sed -i "s@ssh_pub_key@$"(cat $SSH_PUB_KEY)"@" ${dir_pwd}/config-drives/vm1-config/user-data
+sed -i "s@ssh_pub_key@$\"(cat $SSH_PUB_KEY)\"@" ${dir_pwd}/config-drives/vm1-config/user-data
 sed -i "s@ext_int@$VM1_EXTERNAL_IF@" ${dir_pwd}/config-drives/vm1-config/user-data
 sed -i "s@inter_int@$VM1_INTERNAL_IF@" ${dir_pwd}/config-drives/vm1-config/user-data
 # VM2 meta-data
@@ -48,7 +48,7 @@ sed -i "s@manag_mask@$MANAGEMENT_NET_MASK@" ${dir_pwd}/config-drives/vm2-config/
 sed -i "s@manag_broad@${MANAGEMENT_NET}.255@" ${dir_pwd}/config-drives/vm2-config/meta-data
 sed -i "s@dns_vm@$VM_DNS@" ${dir_pwd}/config-drives/vm2-config/meta-data
 # VM2 user-data
-sed -i "s@ssh_pub_key@$"(cat $SSH_PUB_KEY)"@" ${dir_pwd}/config-drives/vm2-config/user-data
+sed -i "s@ssh_pub_key@$\"(cat $SSH_PUB_KEY)\"@" ${dir_pwd}/config-drives/vm2-config/user-data
 sed -i "s@gw_ip@$VM1_INTERNAL_IP@" ${dir_pwd}/config-drives/vm2-config/user-data
 sed -i "s@gw_dev@$VM2_INTERNAL_IF@" ${dir_pwd}/config-drives/vm2-config/user-data
 # Chek folder
