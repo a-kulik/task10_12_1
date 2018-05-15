4ace05c113c31b63a4030d6fb172e6f9461c6fd4
helsik <mymail.kulik@gmail.com>
2018-05-15 10:35:18 +0300

    Update task10_12_1.sh

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -41,8 +41,8 @@ ssh_authorized_keys:
 runcmd:
  - sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
  - sysctl -p > /dev/null
- - iptables -t nat -A POSTROUTING --out-interface ext_int -j MASQUERADE
- - iptables -A FORWARD --in-interface inter_int -j ACCEPT
+ - iptables -t nat -A POSTROUTING --out-interface $VM1_EXTERNAL_IF -j MASQUERADE
+ - iptables -A FORWARD --in-interface $VM1_INTERNAL_IF -j ACCEPT
  - apt-get update
  - apt-get install apt-transport-https ca-certificates curl software-properties-common -y
  - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
